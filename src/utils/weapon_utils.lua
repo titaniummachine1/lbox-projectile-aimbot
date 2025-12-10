fbf28572263604353e04a903ccc53f9fdc0b5b70
titanium_machine <78664175+titaniummachine1@users.noreply.github.com>
2025-12-10 11:45:40 +0100

    src:

--- src/utils/weapon_utils.lua
+++ src/utils/weapon_utils.lua
@@ -3,39 +3,39 @@ local wep_utils = {}
 local old_weapon, lastFire, nextAttack = nil, 0, 0
 
 local function GetLastFireTime(weapon)
-    return weapon:GetPropFloat("LocalActiveTFWeaponData", "m_flLastFireTime")
+	return weapon:GetPropFloat("LocalActiveTFWeaponData", "m_flLastFireTime")
 end
 
 local function GetNextPrimaryAttack(weapon)
-    return weapon:GetPropFloat("LocalActiveWeaponData", "m_flNextPrimaryAttack")
+	return weapon:GetPropFloat("LocalActiveWeaponData", "m_flNextPrimaryAttack")
 end
 
 --- https://www.unknowncheats.me/forum/team-fortress-2-a/273821-canshoot-function.html
 function wep_utils.CanShoot()
-    local player = entities:GetLocalPlayer()
-    if not player then
-        return false
-    end
+	local player = entities:GetLocalPlayer()
+	if not player then
+		return false
+	end
 
-    local weapon = player:GetPropEntity("m_hActiveWeapon")
-    if not weapon or not weapon:IsValid() then
-        return false
-    end
+	local weapon = player:GetPropEntity("m_hActiveWeapon")
+	if not weapon or not weapon:IsValid() then
+		return false
+	end
 
-    if weapon:GetPropInt("LocalWeaponData", "m_iClip1") == 0 then
-        return false
-    end
+	if weapon:GetPropInt("LocalWeaponData", "m_iClip1") == 0 then
+		return false
+	end
 
-    local lastfiretime = GetLastFireTime(weapon)
-    if lastFire ~= lastfiretime or weapon ~= old_weapon then
-        lastFire = lastfiretime
-        nextAttack = GetNextPrimaryAttack(weapon)
-    end
+	local lastfiretime = GetLastFireTime(weapon)
+	if lastFire ~= lastfiretime or weapon ~= old_weapon then
+		lastFire = lastfiretime
+		nextAttack = GetNextPrimaryAttack(weapon)
+	end
 
-    local tickbase = player:GetPropInt("m_nTickBase") * globals.TickInterval()
+	local tickbase = player:GetPropInt("m_nTickBase") * globals.TickInterval()
 
-    old_weapon = weapon
-    return nextAttack <= tickbase
+	old_weapon = weapon
+	return nextAttack <= tickbase
 end
 
 return wep_utils
